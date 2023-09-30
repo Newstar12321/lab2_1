85c9740e797b51858cbb7595e302c4fbaae2a118
Newstar12321 <tazeev.zhenya@mail.ru>
2023-09-30 19:09:57 +0700

    Добавьте файлы проекта.

---
+++ lab2/lab2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int a, b, c, p;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "c = ";
+    cin >> c;
+    p = (b - a) * (c - b);
+    cout << "Произведение длин отрезков ac и ab = " << p << endl;
+}
